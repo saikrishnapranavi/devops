db11f9887cbc6259b15d866f7e640e75f9deee9f
Pranavi <141791444+saikrishnapranavi@users.noreply.github.com>
2024-12-11 21:15:20 +0530

    first.java

--- sample.java
+++ sample.java
@@ -1,4 +1,6 @@
-public class sample {
-	public static void main(String[] args){
-System.out.println("Hello World");
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello world in java");
+        System.out.println("welcome to jenkins");
+    }
 }
